4230ad4acf11ca56b0e4f58febd2d2f35cf62db3
Marvin van Kalsbeek <info@marvinvk.nl>
2019-12-04 15:54:04 +0100

    set some default style configs

---
+++ src/styles/Colors.js
@@ -0,0 +1,8 @@
+module.exports = {
+	primary: '#5A5AFF',
+
+	// grayscales
+	white: '#ffffff',
+	grey: '#cccccc',
+	black: '#000000',
+};
---
+++ src/styles/Sizes.js
@@ -0,0 +1,5 @@
+module.exports = {
+	formElHeight: '40px',
+	gutter: '20px',
+	gutterSmall: '10px',
+};
